5c6aacbb57f852b1f4a23555110b39170f6cf588
Shaun Collins <shaun.collins@evaluagent.com>
2024-05-27 21:57:34 +0100

    added more set values

--- lua/sets.lua
+++ lua/sets.lua
@@ -1,14 +1,26 @@
-local tabSize
-
 vim.opt.relativenumber = true
 
-tabSize = 4
-vim.opt.softtabstop = tabSize
-vim.opt.shiftwidth = tabSize
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.smarttab = true
 
 vim.opt.wrap = true
+vim.opt.list = true
+vim.opt.cursorline = true
 
 vim.opt.incsearch = true
+vim.opt.smartcase = true
 
+vim.opt.encoding = "utf-8"
+vim.opt.syntax = "on"
+
+-- vim.opt.nofoldenable = true
+vim.opt.foldmethod = "indent"
+
+vim.opt.history = 1000
+-- vim.opt.shells = { "/bin/zsh" }
+vim.opt.spell = true
